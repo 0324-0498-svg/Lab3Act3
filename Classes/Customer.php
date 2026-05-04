d6e5ee62a523de0659fd89dff00d82819e8bf6c2
0324-0498-svg <0324-0498@lspu.edu.ph>
2026-05-05 01:34:56 +0800

    Update Customer.php

---
+++ Classes/Customer.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="dashboard.php">Smart Order System</a>
+    <div class="navbar-nav">
+      <a class="nav-link" href="dashboard.php">Dashboard</a>
+      <a class="nav-link" href="customers.php">Customers</a>
+      <a class="nav-link" href="orders.php">New Order</a>
+      <a class="nav-link text-danger" href="logout.php">Logout</a>
+    </div>
+  </div>
+</nav>
