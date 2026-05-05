f090282bc6afdc84253fa9f4043a759b20ed9f6b
0324-0498-svg <0324-0498@lspu.edu.ph>
2026-05-05 16:50:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+    
     $query = "SELECT id, username, password, full_name, role FROM users WHERE username = :username";
     $stmt = $db->prepare($query);
     $stmt->bindParam(':username', $username);
@@ -18,12 +19,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
 
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
+     
     if ($user && ($password === $user['password'] || password_verify($password, $user['password']))) {
+        
         
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['full_name'] = $user['full_name'];
         $_SESSION['username'] = $user['username'];
-        $_SESSION['role'] = $user['role']; 
+        $_SESSION['role'] = $user['role'];  
+
         header("Location: dashboard.php");
         exit;
     } else {
